ad6114bcef629b4b854fa5053f88cd3230bad719
mdatiq123 <mdatiquzzamam1234@gmail.com>
2026-06-23 10:20:45 +0600

    post method api and get method api created

--- index.js
+++ index.js
@@ -1,48 +1,86 @@
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const express = require('express')
-const dotenv = require('dotenv')
-dotenv.config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// ✅ IMPORTANT middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
 
-const app =express()
-const PORT=process.env.PORT
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db("fluxride");
+    const carCollection = db.collection("cars");
+
+    app.get("/cars",async(req,res)=>{
+      const result = await carCollection.find().toArray();
+      res.json(result)
+    })
+
+    app.post("/cars", async (req, res) => {
+      try {
+        const carData = req.body;
+
+        // debug
+        console.log("Received car:", carData);
+
+        if (!carData || Object.keys(carData).length === 0) {
+          return res.status(400).json({
+            success: false,
+            message: "No car data received",
+          });
+        }
+
+        const result = await carCollection.insertOne(carData);
+
+        res.json({
+          success: true,
+          insertedId: result.insertedId,
+        });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({
+          success: false,
+          message: "Insert failed",
+        });
+      }
+    });
+
+    
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    console.log("MongoDB connected successfully!");
+  } catch (error) {
+    console.error(error);
   }
 }
+
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("server is running fine");
+});
 
-app.get("/",(req,res)=>{
-    res.send("server is running fine")
-})
-
-
-
-app.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
